59c2df880f101ee8df081adf7cab8115b9bc94c8
aandresrafael <aandresrafae@gmail.com>
2019-07-17 13:10:27 -0500

    Changed gem using data files provided.

--- lib/etl_aelogica.rb
+++ lib/etl_aelogica.rb
@@ -1,12 +1,13 @@
 require "etl_aelogica/version"
 require 'light-service'
 require 'csv'
+require 'open-uri'
 
 module EtlAelogica
   class Error < StandardError; end
 
   # Organizer
-  class UserEtl
+  class EtlOrganizer
     extend LightService::Organizer
 
     def self.call(args = {})
@@ -15,39 +16,133 @@ module EtlAelogica
 
     def self.actions
       [
-        PullsDataFromCSV,
-        LoadsData
+        RetrievesConnectionUrl,
+        PullsUnitGroupsDataSource,
+        LoadsUnitsGroupsData,
+        LoadsUnitsData
       ]
     end
   end
 
   #Action
-  class PullsDataFromCSV
+  class RetrievesConnectionUrl
     extend LightService::Action
-    promises :retrieved_items
+    promises :base_url
 
     executed do |context|
-      context.retrieved_items = CSV.open(File.join(File.dirname(__FILE__), '../data/users.csv'))
+      context.base_url = 'https://git.appexpress.io/open-source/etl-starter-app/raw/master/data/json'
+    end
+
+  end
+
+  #Action
+  class PullsUnitGroupsDataSource
+    extend LightService::Action
+    promises :unit_groups
+
+    executed do |context|
+      buffer = open("#{context.base_url}/unit_groups.json").read
+      context.unit_groups = JSON.parse(buffer)['unit_groups']
     end
   end
 
   #Action
 
-  class LoadsData
+  class LoadsUnitsGroupsData
     extend LightService::Action
-    expects :retrieved_items
+    expects :unit_groups
 
     executed do |context|
-      context.retrieved_items.each do |item|
-        User.create(
-          first_name: item[0],
-          last_name: item[1],
-          email: item[2],
-          phone_number: item[3],
-          address: item[4],
-          country: item[5]
-        )
+      context.unit_groups.each do |unit_group_hash|
+        invoiceable_fees = unit_group_hash.delete('invoiceable_fees')
+        unit_amenities = unit_group_hash.delete('unit_amenities')
+        discount_plans = unit_group_hash.delete('discount_plans')
+        channel_rate = unit_group_hash.delete('channel_rate')
+        unit_type = unit_group_hash.delete('unit_type')
+        unit_group_hash.delete('scheduled_move_out_ids')
+        unit_group_hash.delete('channel_rate_ids')
+
+        unit_group = UnitGroup.new(unit_group_hash)
+        if channel_rate
+          if cr = ChannelRate.find_by_id(channel_rate['id'])
+            unit_group.channel_rate_id = cr.id
+          else
+            unit_group.channel_rate = ChannelRate.new(channel_rate)
+          end
+        end
+
+        if unit_type
+          if ut = UnitType.find_by_id(unit_type['id'])
+            unit_group.unit_type_id = ut.id
+          else
+            unit_group.unit_type = UnitType.new(unit_type) if unit_type
+          end
+        end
+
+        unit_group.save!
+        #Add invoiceable_fees
+        invoiceable_fees.each do |invoiceable_fee|
+          InvoiceableFee.create(invoiceable_fee)  unless InvoiceableFee.exists?(invoiceable_fee['id'])
+        end
+
+        #Add unit_amenties
+        unit_amenities.each do |unit_amenity|
+          UnitAmenity.create(unit_amenity) unless UnitAmenity.exists?(unit_amenity['id'])
+        end
+
+        #Add discount_plans
+        discount_plans.each do |discount_plan|
+          next if DiscountPlan.exists?(discount_plan['id'])
+          discount_plan_discounts = discount_plan.delete('discount_plan_discounts')
+          discount_plan_controls = discount_plan.delete('discount_plan_controls')
+          client_applications = discount_plan.delete('client_applications')
+          discount_plan.delete('api_association_ids')
+          discount_plan.delete('facility_ids')
+
+          new_discount_plan = DiscountPlan.new(discount_plan)
+          new_discount_plan.save!
+
+          discount_plan_discounts.map {|dp| DiscountPlanDiscount.create(dp) unless DiscountPlanDiscount.exists?(dp['id']) }
+          discount_plan_controls.each do |dpc|
+            dpc.delete('unit_amenity_ids')
+            dpc.delete('discount_plan_ids')
+            DiscountPlanControl.create(dpc) unless DiscountPlanControl.exists?(dpc['id'])
+          end
+          client_applications.map {|ca| ClientApplication.create(ca) unless  ClientApplication.exists?(ca['id'])}
+        end
       end
     end
   end
+
+  class LoadsUnitsData
+    extend LightService::Action
+
+    executed do |context|
+      unit_groups = UnitGroup.pluck(:id)
+      unit_groups.each do |unit_group|
+        buffer = open("#{context.base_url}/#{unit_group}_units.json").read
+        units = JSON.parse(buffer)['units']
+
+        units.each do |unit|
+          unit.delete('channel_rate')
+          unit.delete('unit_amenities')
+          Unit.create(unit)
+        end
+      end
+
+      units_remote = open('https://git.appexpress.io/open-source/etl-starter-app/raw/master/data/csv/units.csv')
+      units_csv = CSV.parse(units_remote, headers: true)
+      units_csv.each do |row|
+        unit = row.to_hash
+        unit.delete('channel_rate')
+        unit.delete('unit_amenities')
+        unit.delete('unit_type')
+        new_unit = Unit.new(unit)
+        new_unit.save!
+      end
+
+    end
+  end
+
+
 end
